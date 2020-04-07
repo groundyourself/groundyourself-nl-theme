bdb8ebcb4b3768c0c919e9dc1566fae1bd17954a
tdemeester <git@tdemeester.nl>
2020-04-07 10:08:48 +0200

    enque style

--- enfold-child/functions.php
+++ enfold-child/functions.php
@@ -4,3 +4,16 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+
+add_action( 'wp_enqueue_scripts', 'dgt_enqueue_styles' );
+function dgt_enqueue_styles() {
+ 
+    $parent_style = 'parent-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
